17a77fdc3cf76d234ce909fb7911dc0b0fdbeaf5
hrm2031 <hrm20311721@gmail.com>
2021-12-10 16:22:10 +0900

    add: template作成
    ルーティング(HOME)も併せて修正

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Portfolio | Home</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Icon -->
+        <script src="https://kit.fontawesome.com/eef91d5f84.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
+        <!-- Style -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;700&family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <header class="header container-lg">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="col-3 site-title">
+                    <h1 class="mb-0">Site-title</h1>
+                </div>
+                <div class="col-9 header-nav">
+                    <nav class="navbar navbar-expand-md navbar-light">
+                        <div class="container justify-content-end">
+                            <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#header-nav" aria-controls="header-nav"
+                                aria-expanded="false" aria-label="Toggle Header menu">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse mt-2 mt-md-0 justify-content-end" id="header-nav">
+                                <ul class="navbar-nav text-end">
+                                    <li class="nav-item ms-3">
+                                        <a class="nav-link" href="#">About</a>
+                                    </li>
+                                    <li class="nav-item ms-3">
+                                        <a class="nav-link" href="#">Skills</a>
+                                    </li>
+                                    <li class="nav-item ms-3">
+                                        <a class="nav-link" href="#">Portfolio</a>
+                                    </li>
+                                    <li class="nav-item ms-3">
+                                        <a class="nav-link" href="#">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <main class="main container-lg">
+            @yield('content')
+        </main>
+        <footer class="footer container">
+            <div class="row justify-content-between align-items-center">
+                <div class="footer-logo d-flex flex-column justify-content-center align-items-center col-4">
+                    <img src="{{ asset('storage/img/Logo.jpg') }}" alt="" class="img-fluid" width="80" height="80">
+                    <small class="d-inline-block">Copyright(C) hrm20311721</small>
+                </div>
+                <div class="footer-menu col-5">
+                    <ul class="footer-list list-unstyled d-flex justify-content-evenly">
+                        <li class="footer-item">
+                            <a href="" class="btn btn-outline-dark border-0 bg-transparent">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github"
+                                    viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="" class="btn btn-outline-dark border-0 bg-transparent">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope"
+                                    viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z" />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
